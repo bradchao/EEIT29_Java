1fbfad565cc6c5309fba432162578007968bb373
brad <brad@brad.tw>
2021-05-24 15:10:14 +0800

    05241510

--- Java2021/src/tw/org/iii/java/Brad22.java
+++ Java2021/src/tw/org/iii/java/Brad22.java
@@ -6,9 +6,9 @@ public class Brad22 {
 		// 陣列 Array => 一堆相同存取特性的值
 		int[] p = new int[6];	// 6 個從 0 開始的 int 元素, 初值皆為 0
 		
-		for (int i=0; i<100; i++) {
-			int point = (int)(Math.random()*6);	// 0 - 5
-			p[point]++;
+		for (int i=0; i<1000000; i++) {
+			int point = (int)(Math.random()*9);	// 0 - 8
+			p[point>5?point-3:point]++;
 		}
 		for (int i=0;i<6; i++) {
 			System.out.println(String.format("%d點出現%d次", (i+1), p[i]));
---
+++ Java2021/src/tw/org/iii/java/Brad23.java
@@ -0,0 +1,24 @@
+package tw.org.iii.java;
+
+public class Brad23 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		// byte, short, int, long, float, double, boolean, char
+		boolean[] b = new boolean[3];
+		System.out.println(b.length);
+		
+		for (int i=0; i<b.length; i++) {
+			System.out.println(b[i]);
+		}
+		
+		
+//		System.out.println("---");
+//		b[1] = 123;
+//		for (int i=0; i<b.length; i++) {
+//			System.out.println(b[i]);
+//		}
+		
+	}
+
+}
