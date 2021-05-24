9179062cce21bd3b3ad46d9f92fa7f780e9826db
brad <brad@brad.tw>
2021-05-24 16:36:21 +0800

    05241636

---
+++ Java2021/src/tw/org/iii/java/Brad28.java
@@ -0,0 +1,15 @@
+package tw.org.iii.java;
+
+public class Brad28 {
+
+	public static void main(String[] args) {
+		int[][][] a = {
+				{{1,2,3},{4,5,6}},
+				{{11,22,33},{44,55,66,77}},
+				{{123,456,789}},
+				{{1,2,3,4,5},{6,7,8,9,10},{123}}
+		};
+		
+	}
+
+}
