24ca6b83ed7856c9196baa2a21f261df2dfcc979
brad <brad@brad.tw>
2021-05-24 11:17:27 +0800

    05231117

---
+++ Java2021/src/tw/org/iii/java/Brad16.java
@@ -0,0 +1,15 @@
+package tw.org.iii.java;
+
+public class Brad16 {
+
+	public static void main(String[] args) {
+
+		// 迴圈 loop => X
+		for (int i=1 ; i<=10 ; i++ ) {
+			System.out.println("Hello, World " + i);
+		}
+		
+		
+	}
+
+}
---
+++ Java2021/src/tw/org/iii/java/Brad17.java
@@ -0,0 +1,13 @@
+package tw.org.iii.java;
+
+public class Brad17 {
+
+	public static void main(String[] args) {
+
+		for ( ; ; ) {
+			System.out.println("Hello, World");
+		}
+		
+	}
+
+}
