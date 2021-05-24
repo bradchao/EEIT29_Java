125b1ca89e47c546e65ba2dc154199a8e272dcd0
brad <brad@brad.tw>
2021-05-24 13:25:46 +0800

    05241325

--- Java2021/src/tw/org/iii/java/Brad99.java
+++ Java2021/src/tw/org/iii/java/Brad99.java
@@ -3,12 +3,14 @@ package tw.org.iii.java;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		int row = 4;
+		int row = 2;
+		int col = 4;
+		int start = 2;
 		
 		for (int k = 0 ; k < row ; k++) {
 			for (int j=1; j<=9; j++) {
-				for (int i=2; i<=5; i++) {
-					int newi = i + k * 4;
+				for (int i=start; i<=start + col -1; i++) {
+					int newi = i + k * col;
 					int r = newi * j;
 					System.out.print(String.format("%d x %d = %d\t", newi, j, r));
 				}
