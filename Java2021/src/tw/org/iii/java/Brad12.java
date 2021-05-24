bb402d2fdeb094b2bdf1d385d831658d27cab415
brad <brad@brad.tw>
2021-05-24 09:23:03 +0800

    05240923

--- Java2021/src/tw/org/iii/java/Brad12.java
+++ Java2021/src/tw/org/iii/java/Brad12.java
@@ -3,8 +3,30 @@ package tw.org.iii.java;
 public class Brad12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int year = 2021;
+		
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					// 潤
+				}else {
+					// 平
+				}
+			}else {
+				// 潤
+			}
+		}else {
+			// 平
+		}
+		// --------------------
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			// 潤
+		}else {
+			// 平
+		}
+		
+		
 	}
 
 }
