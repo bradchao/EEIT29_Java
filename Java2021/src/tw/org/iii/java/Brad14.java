c64eac00ab06b5933b772ca26668f816ecf3d43e
brad <brad@brad.tw>
2021-05-24 09:55:25 +0800

    05240955

---
+++ Java2021/src/tw/org/iii/java/Brad14.java
@@ -0,0 +1,26 @@
+package tw.org.iii.java;
+
+public class Brad14 {
+
+	public static void main(String[] args) {
+		
+		int a = 74;
+		
+		switch (a) {
+			case 1:
+				System.out.println("A");
+				break;
+			case 10:
+				System.out.println("B");
+				break;
+			case 100:
+				System.out.println("C");
+				break;
+			default:
+				System.out.println("XX");
+		}
+		
+		System.out.println("End");
+	}
+
+}
