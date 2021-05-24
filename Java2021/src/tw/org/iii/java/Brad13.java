ea63aca334b8c1e8e4b72dccf0cf7c7b94276a94
brad <brad@brad.tw>
2021-05-24 09:32:09 +0800

    05240932

---
+++ Java2021/src/tw/org/iii/java/Brad13.java
@@ -0,0 +1,16 @@
+package tw.org.iii.java;
+
+public class Brad13 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		
+		if (a >= 10 || b <= 3) {
+			System.out.println(String.format("OK : a = %d, b = %d", a, b));
+		}else {
+			System.out.println(String.format("XX : a = %d, b = %d", a, b));
+		}
+		
+	}
+
+}
