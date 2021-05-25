1dba0845968ccf2bc258e8d21c25e31d5a4f2c76
brad <brad@brad.tw>
2021-05-25 10:35:43 +0800

    05251035

---
+++ Java2021/src/tw/org/iii/java/Brad29.java
@@ -0,0 +1,20 @@
+package tw.org.iii.java;
+
+public class Brad29 {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		while (a < 10) {
+			System.out.println(a++);
+		}
+		System.out.println("---");
+		
+		int b = 1;
+		for (;b < 10;) {
+			System.out.println(b++);
+		}
+		
+	}
+
+}
---
+++ Java2021/src/tw/org/iii/java/Brad30.java
@@ -0,0 +1,13 @@
+package tw.org.iii.java;
+
+public class Brad30 {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		do {
+			System.out.println(a++);
+		} while (a < 10);
+	}
+
+}
