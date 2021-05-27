9acd0285e088ea897c9e997ed43eb18429e3e8b4
brad <brad@brad.tw>
2021-05-27 10:37:19 +0800

    05271037

--- Java2021/src/tw/org/iii/myclass/TWId.java
+++ Java2021/src/tw/org/iii/myclass/TWId.java
@@ -24,11 +24,18 @@ public class TWId {
 		// 2. 第一碼 ABC...XYZ
 		// 3. 第二碼 1, 2
 		// 4. 第 3 - 10 碼: 0 - 9
-		
 		boolean isCheckOK = false;
+		String letters = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
 		if (id.matches("[A-Z][12][0-9]{8}")) {
 			// 驗證碼程序
-				
+			char c1 = id.charAt(0);
+			int n12 = letters.indexOf(c1) + 10;
+			int n1 = n12 / 10;
+			int n2 = n12 % 10;
+			
+			
+			
+			
 			
 			
 		}
