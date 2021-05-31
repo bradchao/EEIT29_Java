1bc40b92ec5739ea57dc35ccf88d84f441d5b3ac
brad <brad@brad.tw>
2021-05-31 14:00:49 +0800

    05311400

--- Java2021/src/tw/org/iii/java/Brad50.java
+++ Java2021/src/tw/org/iii/java/Brad50.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Brad50 extends JFrame{
+public class Brad50 extends JFrame implements ActionListener {
 	private JButton b1, b2, b3;
 	
 	public Brad50() {
@@ -20,13 +20,47 @@ public class Brad50 extends JFrame{
 		setLayout(new FlowLayout());
 		add(b1); add(b2); add(b3);
 		
-		MyListener myListener = new MyListener();
-		b1.addActionListener(myListener);
-		b2.addActionListener(myListener);
+//		MyListener myListener = new MyListener();
+//		b1.addActionListener(myListener);
+//		b2.addActionListener(myListener);
+		
+//		b1.addActionListener(this);
+//		b2.addActionListener(this);
+//		b3.addActionListener(this);
+		
+		ButtonListener bListener = new ButtonListener();
+		b1.addActionListener(bListener);
+		b2.addActionListener(bListener);
+		b3.addActionListener(bListener);
+		
 		
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//System.out.println("OK:" + e.getActionCommand());
+		if (e.getSource() == b1) {
+			System.out.println("b1");
+		}else if (e.getSource() == b2) {
+			System.out.println("b2");
+		}else if (e.getSource() == b3) {
+			System.out.println("b3");
+		}
+	}
+	
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == b1) {
+				System.out.println("b1");
+			}else if (e.getSource() == b2) {
+				System.out.println("b2");
+			}else if (e.getSource() == b3) {
+				System.out.println("b3");
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -37,7 +71,11 @@ public class Brad50 extends JFrame{
 
 class MyListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("OK1");
+		System.out.println("OK:" + e.getActionCommand());
+//		if (e.getSource() == b1) {
+//			
+//		}
+		
 	}
 }
 
