4125596c7f8b647f35264b8bb15d3b1d8007d93e
brad <brad@brad.tw>
2021-05-31 11:19:31 +0800

    05311119

--- Java2021/src/tw/org/iii/java/Brad49.java
+++ Java2021/src/tw/org/iii/java/Brad49.java
@@ -3,9 +3,27 @@ package tw.org.iii.java;
 public class Brad49 {
 
 	public static void main(String[] args) {
-
+		RecShape obj1 = new RecShape(10, 3);
+		System.out.println(obj1.getArea());
+		System.out.println(obj1.getLength());
+		
+		CircleShape obj2 = new CircleShape();
+		System.out.println(obj2.getArea());
+		
+		TriShape obj3 = new TriShape();
+		System.out.println(obj3.getArea());
+		
+		System.out.println("-------------");
+		System.out.println(sum2Shape(obj2, obj3));
+		
 	}
 
+	static double sum2Shape(Shape s1, Shape s2) {
+		double ret = s1.getArea() + s2.getArea();
+		return ret;
+	}
+	
+	
 }
 
 // 規格
@@ -16,16 +34,18 @@ interface Shape {	// public
 	double getLength();	// public
 }
 class RecShape implements Shape {
+	private double w, h;
+	RecShape(double w, double h){this.w=w; this.h=h;}
 	public double getArea() { 
-		return 0.0;
+		return w*h;
 	}
 	public double getLength() {
-		return 0.0;
+		return (w+h)*2;
 	}
 }
 class CircleShape implements Shape {
 	public double getArea() { 
-		return 0.0;
+		return 34;
 	}
 	public double getLength() {
 		return 0.0;
@@ -34,7 +54,7 @@ class CircleShape implements Shape {
 
 class TriShape implements Shape {
 	public double getArea() { 
-		return 0.0;
+		return 45;
 	}
 	public double getLength() {
 		return 0.0;
