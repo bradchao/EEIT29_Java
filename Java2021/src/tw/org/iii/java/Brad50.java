3d114001cc133cc4666779503555011c083ad5cc
brad <brad@brad.tw>
2021-05-31 11:50:11 +0800

    05311150

--- Java2021/src/tw/org/iii/java/Brad50.java
+++ Java2021/src/tw/org/iii/java/Brad50.java
@@ -1,12 +1,22 @@
 package tw.org.iii.java;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Brad50 extends JFrame{
+	private JButton b1, b2, b3;
 	
 	public Brad50() {
 		super("偶的視窗程式");
 		
+		b1 = new JButton("B1");
+		b2 = new JButton("B2");
+		b3 = new JButton("B3");
+		
+		setLayout(new FlowLayout());
+		add(b1); add(b2); add(b3);
 		
 		setSize(640, 480);
 		setVisible(true);
