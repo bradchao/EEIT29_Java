ffc9a4406c4bde567c98ba46447890f6b00b7e38
brad <brad@brad.tw>
2021-06-01 15:11:55 +0800

    06011511

--- Java2021/src/tw/org/iii/java/MyEditor.java
+++ Java2021/src/tw/org/iii/java/MyEditor.java
@@ -1,11 +1,35 @@
 package tw.org.iii.java;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class MyEditor extends JFrame {
+	private JButton openBtn, newBtn, saveBtn, saveAsBtn;
+	private JTextArea jTextArea;
 	
 	public MyEditor() {
 		super("Brad 文字編輯器");
+		
+		setLayout(new BorderLayout());
+		
+		JPanel topLine = new JPanel(new FlowLayout());
+		openBtn = new JButton("Open");
+		newBtn = new JButton("New");
+		saveBtn = new JButton("Save");
+		saveAsBtn = new JButton("Save as");
+		topLine.add(newBtn);topLine.add(openBtn);
+		topLine.add(saveBtn);topLine.add(saveAsBtn);
+		
+		add(topLine, BorderLayout.NORTH);
+		
+		jTextArea = new JTextArea();
+		add(jTextArea, BorderLayout.CENTER);
+		
 		
 		setSize(640, 480);
 		setVisible(true);
