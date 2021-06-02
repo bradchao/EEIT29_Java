c95d8733c61ab1f77347bce22403f87bd1d7b376
brad <brad@brad.tw>
2021-06-02 14:56:22 +0800

    06021456

--- Java2021/src/tw/org/iii/myclass/MyDrawer.java
+++ Java2021/src/tw/org/iii/myclass/MyDrawer.java
@@ -1,11 +1,27 @@
 package tw.org.iii.myclass;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
 	public MyDrawer() {
 		setBackground(Color.YELLOW);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		//System.out.println(g instanceof Graphics2D);
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.BLUE);
+		g2d.drawLine(0, 0, 200, 200);
+		
+		
 	}
 }
