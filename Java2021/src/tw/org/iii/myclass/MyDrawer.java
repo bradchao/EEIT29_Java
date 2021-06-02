3d8dd06e0b09b922e1b2f9cc9f0828c4348d84f9
brad <brad@brad.tw>
2021-06-02 15:59:09 +0800

    06021558

--- Java2021/src/tw/org/iii/myclass/MyDrawer.java
+++ Java2021/src/tw/org/iii/myclass/MyDrawer.java
@@ -5,11 +5,15 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
+	private LinkedList<HashMap<String, Integer>> line = new LinkedList<>();
+	
 	public MyDrawer() {
 		setBackground(Color.YELLOW);
 		
@@ -26,14 +30,25 @@ public class MyDrawer extends JPanel {
 		//Graphics2D g2d = (Graphics2D)g;
 		
 		g.setColor(Color.BLUE);
-		g.drawLine(0, 0, 200, 200);
+		for (int i=1; i<line.size(); i++) {
+			HashMap<String, Integer> p0 = line.get(i-1);
+			HashMap<String, Integer> p1 = line.get(i);
+			System.out.println("" + p0.get("x") + "," +p0.get("y") + "," + p1.get("x") + "," + p1.get("y"));
+			g.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));
+		}
 	}
 	
 	private class MyMouseListener extends MouseAdapter {
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			super.mouseDragged(e);
-			System.out.println(e.getX() + "," + e.getY());
+			//System.out.println(e.getX() + "," + e.getY());
+			
+			HashMap<String, Integer> point = new HashMap<>();
+			point.put("x", e.getX()); point.put("y", e.getY());
+			
+			line.add(point);
+			invalidate();
 		}
 	}
 	
