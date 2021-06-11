6bbb5be044f9e46309615d20d22fa080b764c365
brad <brad@brad.tw>
2021-06-11 10:51:45 +0800

    06111051

--- Java2021/src/tw/org/iii/java/MySign.java
+++ Java2021/src/tw/org/iii/java/MySign.java
@@ -2,17 +2,22 @@ package tw.org.iii.java;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import tw.org.iii.myclass.MyDrawer;
 
 public class MySign extends JFrame {
-	private JButton clear; 
+	private JButton clear, undo, redo, save; 
 	private MyDrawer myDrawer;
 	
 	public MySign() {
@@ -32,6 +37,16 @@ public class MySign extends JFrame {
 		JPanel topLine = new JPanel(new FlowLayout());
 		clear = new JButton("Clear");
 		topLine.add(clear);
+
+		undo = new JButton("Undo");
+		topLine.add(undo);
+
+		redo = new JButton("Redo");
+		topLine.add(redo);
+		
+		save = new JButton("Save");
+		topLine.add(save);
+		
 		add(topLine, BorderLayout.NORTH);
 		
 		myDrawer = new MyDrawer();
@@ -44,6 +59,38 @@ public class MySign extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				myDrawer.clear();
+			}
+		});
+		
+		undo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.undo();
+			}
+		});
+		
+		redo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.redo();
+			}
+		});
+		
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				BufferedImage image = new BufferedImage(myDrawer.getWidth(), myDrawer.getHeight(), 
+						BufferedImage.TYPE_INT_RGB);
+				Graphics2D g2d = image.createGraphics();
+				myDrawer.paint(g2d);
+				try {
+					ImageIO.write(image, "jpeg", new File("dir1/brad.jpg"));
+					JOptionPane.showMessageDialog(null, "Save JPEG OK");
+				}catch(Exception e2) {
+					System.out.println(e2.toString());
+				}
 			}
 		});
 	}
