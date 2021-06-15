941b9f30ff63554b82607b687952d2a27f858bf7
brad <brad@brad.tw>
2021-06-15 14:03:26 +0800

    06151403

--- Java2021/src/tw/org/iii/java/Brad80.java
+++ Java2021/src/tw/org/iii/java/Brad80.java
@@ -1,7 +1,13 @@
 package tw.org.iii.java;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -20,6 +26,52 @@ public class Brad80 extends JFrame {
 	}
 	
 	private class MyPanel extends JPanel {
+		private BufferedImage ballImg = null;
+		private Timer timer;
+		private int x, y, viewW, viewH, dx, dy, ballW, ballH;
+		
+		public MyPanel() {
+			dx = dy = 8;
+			
+			timer = new Timer();
+			try {
+				ballImg = ImageIO.read(new File("dir1/ball3.png"));
+				ballW = ballImg.getWidth();
+				ballH = ballImg.getHeight();
+			}catch(Exception e) {
+				
+			}
+			timer.schedule(new MyTask(), 1000, 60);
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+
+			viewW = getWidth();
+			viewH = getHeight();
+
+			
+			if (ballImg == null) return;
+			
+			g.drawImage(ballImg, x, y,  null);
+			
+		}
+		
+		private class MyTask extends TimerTask {
+			@Override
+			public void run() {
+				if (x < 0 || x  + ballW > viewW) {
+					dx *= -1;
+				}
+				if (y<0 || y + ballH > viewH ) {
+					dy *= -1;
+				}
+				
+				x += dx; y += dy;
+				repaint();
+			}
+		}
 		
 	}
 
