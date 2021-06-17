48894b73e1c9cf86f2807ebfef36fb16b550b8d6
brad <brad@brad.tw>
2021-06-17 10:39:07 +0800

    06171039

--- Java2021/src/tw/org/iii/java/Brad91.java
+++ Java2021/src/tw/org/iii/java/Brad91.java
@@ -3,17 +3,21 @@ package tw.org.iii.java;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
 
 public class Brad91 {
+	static final String sqlCheckRepeat = "SELECT count(*) as nums FROM member WHERE account = ?";
 	static final String sqlAppend = "INSERT INTO member (account,passwd,realname) VALUES (?,?,?)";
-	static PreparedStatement stmtAppend;
+	static PreparedStatement stmtAppend, stmtCheckRepeat;
 	
 	public static void main(String[] args) {
 		String account = JOptionPane.showInputDialog("Account");
 		String passwd = JOptionPane.showInputDialog("Password");
+		String realname = JOptionPane.showInputDialog("Realname");
 		
 		String url = "jdbc:mysql://localhost:3306/newiii";
 		Properties prop = new Properties();
@@ -21,9 +25,15 @@ public class Brad91 {
 		prop.put("password", "root");
 		
 		try (Connection conn = DriverManager.getConnection(url, prop)) {
+			stmtCheckRepeat = conn.prepareStatement(sqlCheckRepeat);
 			stmtAppend = conn.prepareStatement(sqlAppend);
+			
 			if (!isDataRepeat(account)) {
-				
+				if (appendData(account, passwd, realname)) {
+					JOptionPane.showMessageDialog(null, "資料新增成功");
+				}else {
+					JOptionPane.showMessageDialog(null, "資料新增錯誤");
+				}
 			}else {
 				JOptionPane.showMessageDialog(null, "資料重複錯誤");
 			}
@@ -34,8 +44,19 @@ public class Brad91 {
 		
 	}
 	
-	static boolean isDataRepeat(String account) {
-		return true;
+	static boolean isDataRepeat(String account) throws SQLException {
+		stmtCheckRepeat.setString(1, account);
+		ResultSet rs = stmtCheckRepeat.executeQuery();
+		rs.next();
+		return rs.getInt("nums") != 0;
+	}
+	
+	static boolean appendData(String account, String passwd, String realname) throws SQLException {
+		stmtAppend.setString(1, account);
+		stmtAppend.setString(2, passwd);
+		stmtAppend.setString(3, realname);
+		int rowCount = stmtAppend.executeUpdate();
+		return rowCount != 0;
 	}
 
 }
