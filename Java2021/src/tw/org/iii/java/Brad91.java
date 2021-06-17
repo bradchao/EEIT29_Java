00c96764e70d91e29794281933e1b199ec913b54
brad <brad@brad.tw>
2021-06-17 10:23:15 +0800

    06171023

--- Java2021/src/tw/org/iii/java/Brad91.java
+++ Java2021/src/tw/org/iii/java/Brad91.java
@@ -5,28 +5,37 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.Properties;
 
+import javax.swing.JOptionPane;
+
 public class Brad91 {
 	static final String sqlAppend = "INSERT INTO member (account,passwd,realname) VALUES (?,?,?)";
 	static PreparedStatement stmtAppend;
 	
 	public static void main(String[] args) {
-
+		String account = JOptionPane.showInputDialog("Account");
+		String passwd = JOptionPane.showInputDialog("Password");
+		
 		String url = "jdbc:mysql://localhost:3306/newiii";
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
 		
 		try (Connection conn = DriverManager.getConnection(url, prop)) {
-			 stmtAppend = conn.prepareStatement(sqlAppend);
-			
+			stmtAppend = conn.prepareStatement(sqlAppend);
+			if (!isDataRepeat(account)) {
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "資料重複錯誤");
+			}
 			
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
 		
-		
-		
-		
+	}
+	
+	static boolean isDataRepeat(String account) {
+		return true;
 	}
 
 }
